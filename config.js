d1df1a09ca9f0b06d2379f4d52d2aac9c8511883
ciqulover <ciqulover@gmail.com>
2018-10-24 23:08:52 +0800

    rewrite

--- config.js
+++ config.js
@@ -1,28 +1,18 @@
-const config = {
-  // 服务端端口，需要与disqus-proxy前端设置一致
-  port: 5509,
+const isDev = process.env.NODE_ENV === 'DEV'
 
-  // 你的diqus secret key
-  api_secret: 'your secret key',
+const config = isDev ? require('./dev.config.json') : require('./config.json')
 
-  // 你的website的 shortname 名称 比如在你的disqus安装代码中 有这样一句脚本：
-  // s.src = 'https://abcd.disqus.com/embed.js';
-  // 那么你的disqus 的shortname 就是 abcd
-  shortname: 'your short name',
-
-  // 服务端socks5代理转发，便于在本地测试，生产环境通常为null
-  // socks5Proxy: {
-  //   host: 'localhost',
-  //   port: 1086
-  // },
-
-  socks5Proxy: null,
-
-  // 日志输出位置,输出到文件或控制台 'file' | 'console'
-  log: 'console'
+if (isDev) {
+  config.socks5Proxy = {
+    host: 'localhost',
+    port: 1086
+  }
 }
 
-// Docker启动时覆盖参数
+// 这个API_key是disqus专门用来匿名评论的公有API_key
+config.api_key = 'E8Uh5l5fHZ6gD8U3KycjAIAk46f68Zw7C6eW8WSjZvCLXebZ7p0r1yrYDrLilk2F'
+
+// 如果用Docker启动，覆盖参数
 if (process.env.PORT) config.port = process.env.PORT
 if (process.env.API_SECRECT) config.api_secret = process.env.API_SECRECT
 if (process.env.SHORT_NAME) config.shortname = process.env.SHORT_NAME
---
+++ router/index.js
@@ -0,0 +1,99 @@
+const router = require('koa-router')()
+const fetch = require('node-fetch')
+const wrappedFetch = require('socks5-node-fetch')
+const {URLSearchParams} = require('url')
+
+const logger = require('../lib/logger')
+const {api_key, api_secret, shortname, socks5Proxy} = require('../config')
+
+let request = fetch
+
+if (socks5Proxy && socks5Proxy.host && socks5Proxy.port) {
+  request = wrappedFetch({
+    socksHost: socks5Proxy.host,
+    socksPort: socks5Proxy.port
+  })
+}
+
+router.get('/api/getThreads', async function (ctx) {
+  const identifier = ctx.request.query.identifier
+  logger.info('Get thread with identifier: ' + identifier)
+
+  const url = 'https://disqus.com/api/3.0/threads/list.json?' +
+    'api_secret=' + api_secret +
+    '&forum=' + shortname +
+    '&thread:ident=' + encodeURIComponent(identifier)
+
+  let result
+
+  try {
+    result = await request(url)
+    result = await result.json()
+    logger.info('Get thread successfully with response code: ' + result.code)
+  } catch (e) {
+    result = unexpectedError(e)
+    logger.error('Error when get thread:' + e.message)
+  }
+  ctx.body = result
+})
+
+router.get('/api/getComments', async function (ctx) {
+  const identifier = ctx.request.query.identifier
+  const cursor = ctx.request.query.cursor
+  logger.info('Get Comments with identifier: ' + identifier)
+
+  let result
+  const url = 'https://disqus.com/api/3.0/threads/listPosts.json?' +
+    'api_secret=' + api_secret +
+    '&forum=' + shortname +
+    '&limit=' + 25 +
+    `${cursor ? '&cursor=' + cursor : ''}` +
+    '&thread:ident=' + encodeURIComponent(ctx.request.query.identifier)
+
+  try {
+    result = await request(url)
+    result = await result.json()
+    logger.info('Get comments successfully with response code: ' + result.code)
+  } catch (e) {
+    result = unexpectedError(e)
+    logger.error('Error when get comment:' + e.message)
+  }
+
+  ctx.body = result
+})
+
+router.post('/api/createComment', async function (ctx) {
+  const body = ctx.request.body
+  logger.info('Create comment: ' + JSON.stringify(body))
+  const {message, thread, author_name, author_email, parent} = body
+  const params = new URLSearchParams()
+  params.append('api_key', api_key)
+  params.append('parent', parent)
+  params.append('message', message)
+  params.append('thread', thread)
+  params.append('author_name', author_name)
+  params.append('author_email', author_email)
+  let result
+  try {
+    result = await request('https://disqus.com/api/3.0/posts/create.json', {
+      method: 'POST',
+      body: params
+    })
+    result = await result.json()
+    logger.info('Create comment successfully with response code: ' + result.code)
+  } catch (e) {
+    result = unexpectedError(e)
+    logger.error('Error when create comment:' + e.message)
+  }
+  ctx.body = result
+})
+
+module.exports = router
+
+
+function unexpectedError(error) {
+  return {
+    code: 'RPC CALL ERROR',
+    response: error.message
+  }
+}
